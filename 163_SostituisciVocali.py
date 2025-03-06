817c4f176b199b4a8fd4856f6c58a11e4893c7fb
GianoG8002 <gianotti.gabriele@itisgrassi.edu.it>
2025-03-06 09:38:43 +0100

    Add files via upload

--- 163_SostituisciVocali.py
+++ 163_SostituisciVocali.py
@@ -1,1 +1,5 @@
 s = input('inserire una stringa: ')
+l = ['A','a','E','e','I','i','O','o','U','u']
+for i in range(len(l)):
+    s = s.replace(l[i],'*')
+print(s)
---
+++ 164_SostituisciNome.py
@@ -0,0 +1,4 @@
+s = 'Marco ieri è andato in piscina mentre Marco domani andrà in montagna.'
+n = input('inserire il proprio nome: ')
+s = s.replace('Marco',n)
+print(s)
